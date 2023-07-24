26961a4c5840055ec8d13439c2e7a26304169de9
trueToastedCode <lennard2hp@gmail.com>
2023-07-24 15:06:38 +0200

    Another attempt to fix the insufficient funds error

--- src/strategy/my_strategy.py
+++ src/strategy/my_strategy.py
@@ -120,24 +120,14 @@ class MyStrategy:
         # oco order
         amount = self.my_binance.exchange.amount_to_precision(
             self.config.CCXT_SYMBOL,
-            float(market_order_status['info']['executedQty'])
+            self.my_binance.get_free_balance(self.config.CCXT_SYMBOL_A)
+            if self.config.OCO_FETCH_BALANCE
+            else float(market_order_status['info']['executedQty'])
         )
         logging.debug(f'Making oco order [ amount: {amount}, tp: {take_profit}, sl: {stop_loss} ]')
 
-        is_funds_error = False
-        while True:
-            try:
-                oco_order = self.my_binance.oco_order(amount, 'sell', take_profit, stop_loss, stop_loss)
-                break
-            except ccxt.base.errors.InsufficientFunds:
-                if is_funds_error:
-                    raise RuntimeError('Insufficient funds')
-                logging.warning('Insufficient funds, try with with free balance instead of executed quantity again')
-                is_funds_error = True
-                amount = self.my_binance.exchange.amount_to_precision(
-                    self.config.CCXT_SYMBOL,
-                    self.my_binance.get_free_balance(self.config.CCXT_SYMBOL_A)
-                )
+        oco_order = self.my_binance.oco_order(amount, 'sell', take_profit, stop_loss, stop_loss)
+
         self.last_oco_ids = [item['orderId'] for item in oco_order['orders']]
 
         logging.info('Open Long ok')
